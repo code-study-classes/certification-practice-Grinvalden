27f13dacc412aeed227c540ceeafb0049f9cc145
Grinvalden <1515@dr.com>
2023-10-31 21:14:45 +0300

    !

--- numbers.js
+++ numbers.js
@@ -1,1 +1,4 @@
-
+const calculateDistance = (a, b) => b - a;
+console.log(calculateDistance(0, 5));
+const calculateSegmentProduct = (a, b, c) => (c < a || c > b ? false : (c - a) * (b - c));
+console.log(calculateSegmentProduct(-5, 5, 0));
